3bc38b882c5e4a81e4d8a556dc5092f849dcb159
aDesign-su <Blizz72@yandex.ru>
2024-04-09 04:03:20 +0500

    begin project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { GeneralComponent } from './content/general/general.component';
+import { HeaderComponent } from './content/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+const appRoutes: Routes = [
+  { path: '', component: GeneralComponent },
+  // { path: 'sticks', component: HomeComponent, canActivate: [AuthGuard] },
+  // { path: 'device', component: HomeComponent, canActivate: [AuthGuard] },
+  // { path: 'about', component: HomeComponent, canActivate: [AuthGuard] },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralComponent,
+    HeaderComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
